6823a1480ccbb2f980d4c9dfc15992383af7ced4
Sharixx <cis.vlad74@gmail.com>
2021-06-10 19:19:53 +0300

    #1

--- main.py
+++ main.py
@@ -8,5 +8,10 @@ def index():
     return render_template(index.html)
 
 
+@app.route('/about')
+def about():
+    return render_template(about.html)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
