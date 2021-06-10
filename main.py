a9ff8dbdc57626d82f0cad58c5bc90753ac4e9e4
Orest-Programmer <orestmusicstudio@gmail.com>
2021-06-10 20:04:08 +0300

    I Commited test

--- main.py
+++ main.py
@@ -8,6 +8,17 @@ def index():
     return render_template(index.html)
 
 
+
+
+@app.route('/about')
+def about():
+    return render_template(about.html)
+@app.route('/about')
+def about():
+    return render_template(about.html)
+@app.route('/about')
+def about():
+    return render_template(about.html)
 @app.route('/about')
 def about():
     return render_template(about.html)
