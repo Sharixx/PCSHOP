fcb7dcdbb5f293a26d06d56958b19c96d8f04722
Sharixx <cis.vlad74@gmail.com>
2021-06-10 21:37:15 +0300

    #2

--- main.py
+++ main.py
@@ -5,27 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template(index.html)
+    return render_template('index.html')
 
 
-@app.route('/about')
-def about():
-    return render_template(about.html)
-
-
-@app.route('/about')
-def about():
-    return render_template(about.html)
-
-
-@app.route('/about')
-def about():
-    return render_template(about.html)
-
-
-@app.route('/about')
-def about():
-    return render_template(about.html)
+@app.route('/base')
+def base():
+    return render_template('base.html')
 
 
 if __name__ == "__main__":
