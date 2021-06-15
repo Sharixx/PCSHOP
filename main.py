6658574fd44fd3cfb3022e1415215e05f53d2401
Sharixx <cis.vlad74@gmail.com>
2021-06-15 15:29:35 +0300

    #2

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
 
 app = Flask(__name__)
 
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('remember me')
 
 @app.route('/')
 def index():
@@ -40,7 +49,9 @@ def basket():
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+
+    return render_template('login.html', form=form)
 
 
 @app.route('/register')
